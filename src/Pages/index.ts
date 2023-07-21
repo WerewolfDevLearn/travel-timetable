ce17f5fcf03eb6b3f7a2ba4751b878caea7ea079
Mikhail Khodak <werewolfdevlearn@gmail.com>
2023-07-21 18:21:29 +0300

    fix path to commponents

--- src/Pages/index.ts
+++
@@ -1,6 +0,0 @@
-import { ErrorPage } from './ErrorPage';
-import { HomePage } from './HomePage';
-import { LoginPage } from './LoginPage';
-import { RegisterPage } from './RegisterPage';
-import { TripsPage } from './TripsPage';
-export { ErrorPage, HomePage, LoginPage, RegisterPage, TripsPage };
